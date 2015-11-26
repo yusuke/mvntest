09a8a2f66715ead511e3e2ac7bd87ee1a4161ed3
Yusuke Yamamoto <yusuke@mac.com>
2015-11-27 01:02:25 +0900

    initial commit

---
+++ src/main/java/org/mvntest/Hello.java
@@ -0,0 +1,10 @@
+package org.mvntest;
+
+/**
+ * Created by yusuke on 11/27/15.
+ */
+public class Hello {
+    public static void main(String... args) {
+        System.out.println("Hello World");
+    }
+}
